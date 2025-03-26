aa69096efa27f6897942f652ff4bf298633942d8
u0911145 <u0911145@utah.edu>
2025-03-25 18:29:06 -0600

    Round robin working.

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ log = core.getLogger()
 switch_ip = IPAddr("10.0.0.10")
 switch_mac = EthAddr("00:00:00:00:00:10")
 
-servers = [(IPAddr("10.0.0.5"), EthAddr("00:00:00:00:00:05")),
-           (IPAddr("10.0.0.6"), EthAddr("00:00:00:00:00:06"))]
+servers = [(IPAddr("10.0.0.5"), EthAddr("00:00:00:00:00:05"), 5),
+           (IPAddr("10.0.0.6"), EthAddr("00:00:00:00:00:06"), 6)]
 
 server_index = 0
 client_server_map = {}
@@ -44,6 +44,13 @@ class LoadBalancer (object):
             log.info("Handling IP packet")
             self.handle_ip(packet, event)
             return
+        
+    def assign_client_server(self, client_key):
+        global client_server_map
+        global server_index
+        client_server_map[client_key] = servers[server_index]
+        server_index = (server_index + 1) % len(servers)
+        return client_server_map[client_key]
 
     def handle_arp(self, packet, event):
         global server_index, client_server_map
@@ -51,19 +58,25 @@ class LoadBalancer (object):
 
         if arp_packet.opcode == pkt.arp.REQUEST:
             requested_ip = arp_packet.protodst
-            chosen_server_mac = EthAddr("00:00:00:00:00:01")
+            client_ip = arp_packet.protosrc
+            client_mac = arp_packet.hwsrc
+
+            chosen_server_ip = switch_ip
+            chosen_server_mac = switch_mac
 
             if requested_ip == switch_ip:
                 log.info("Received ARP request for virtual IP")
-                client_ip = arp_packet.protosrc
-                if client_ip not in client_server_map:
-                    client_server_map[client_ip] = servers[server_index]
-                    server_index = (server_index + 1) % len(servers)
-                chosen_server_ip, chosen_server_mac = client_server_map[client_ip]
+                chosen_server_mac = self.assign_client_server((client_ip, client_mac))[1]
                 chosen_server_ip = switch_ip
             else:
                 log.info(f"Received ARP request for client IP {requested_ip}")
-                chosen_server_ip = requested_ip
+                # Request is from server, get client ip and client map
+                for client_key in client_server_map:
+                    log.info(f"Client key: {client_key}, Requested IP: {requested_ip}")
+                    if client_key[0] == requested_ip:
+                        log.info(f"Found client key: {client_key}")
+                        chosen_server_ip, chosen_server_mac = client_key
+                        break
             
             arp_reply = pkt.arp()
             arp_reply.hwsrc = chosen_server_mac
@@ -82,7 +95,7 @@ class LoadBalancer (object):
 
             msg = of.ofp_packet_out()
             msg.data = ether.pack()
-            msg.actions.append(of.ofp_action_output(port=of.OFPP_ALL))
+            msg.actions.append(of.ofp_action_output(port=event.port))
             event.connection.send(msg)
 
     def handle_ip(self, packet, event):
@@ -90,25 +103,23 @@ class LoadBalancer (object):
     
         ip_packet = packet.payload
         client_ip = ip_packet.srcip
+        client_mac = packet.src
     
         if packet.next.dstip == switch_ip:
-            if client_ip in client_server_map:
-                chosen_server_ip, chosen_server_mac = client_server_map[client_ip]
-            else:
-                chosen_server_ip, chosen_server_mac = servers[server_index]
-                client_server_map[client_ip] = (chosen_server_ip, chosen_server_mac)
+            chosen_server_ip, chosen_server_mac, chosen_port = self.assign_client_server((client_ip, client_mac))
             
-            log.info(f"Forwarding IP packet from {packet.next.srcip} to {chosen_server_ip}")
+            log.info(f"Forwarding IP packet from {packet.next.srcip} to {chosen_server_ip} via port {chosen_port}")
             
             # Client -> Server
             msg = of.ofp_flow_mod()
             msg.match.dl_type = pkt.ethernet.IP_TYPE
             msg.match.nw_dst = switch_ip
+            msg.match.in_port = event.port
             
             # Actions
             msg.actions.append(of.ofp_action_nw_addr.set_dst(chosen_server_ip))
             msg.actions.append(of.ofp_action_dl_addr.set_dst(chosen_server_mac))
-            msg.actions.append(of.ofp_action_output(port=of.OFPP_ALL))
+            msg.actions.append(of.ofp_action_output(port=chosen_port))
             
             event.connection.send(msg)
             
@@ -116,11 +127,12 @@ class LoadBalancer (object):
             msg = of.ofp_flow_mod()
             msg.match.dl_type = pkt.ethernet.IP_TYPE
             msg.match.nw_src = chosen_server_ip
+            msg.match.in_port = chosen_port
             
             # Actions
             msg.actions.append(of.ofp_action_nw_addr.set_src(switch_ip))
             msg.actions.append(of.ofp_action_dl_addr.set_src(switch_mac))
-            msg.actions.append(of.ofp_action_output(port=of.OFPP_ALL))
+            msg.actions.append(of.ofp_action_output(port=event.port))
             
             event.connection.send(msg)
 
